c9347e30f5d696eefcd603071d26ee818b327ef6
VidgarVii <vidasp@mail.ru>
2018-12-12 09:10:54 +0300

    add target_blank&refact time zone

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module TestGuru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.time_zone = 'Europe/Moscow'
+    config.time_zone = 'Moscow'
     config.i18n.default_locale = :ru
 
     # Settings in config/environments/* take precedence over those specified here.
