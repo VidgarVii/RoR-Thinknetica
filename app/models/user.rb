9aca12991ff7e1e429327f62af71171469b3ab73
VidgarVii <vidasp@mail.ru>
2018-12-15 17:08:11 +0300

    current tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  attr_reader :current_tests
+
+  def current_test(test)
+    @current_tests ||= []
+    @current_tests << test
+  end
+
+  def current_test_whis_level(level)
+    @current_tests.select { |test| test.level == level}
+  end
 end
