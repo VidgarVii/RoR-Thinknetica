1494088e4fb2d9caec79e8c34baeda359edd5045
VidgarVii <vidasp@mail.ru>
2018-12-17 21:32:14 +0300

    refact methods in User & Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,12 +1,12 @@
 class Test < ApplicationRecord
-  has_many :questions
+  has_many :questions, dependent: :destroy
   has_many :results
   has_many :users, through: :results
   belongs_to :category
   belongs_to :user
 
   def self.all_with_category(category)
-    joins("INNER JOIN categories ON tests.category_id = categories.id")
+    joins(:category)
         .where(categories: {title: category})
         .order(title: :desc)
         .pluck(:title)
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,41 +6,41 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create!([
+users = User.create!([
   {name: 'Admin', email: 'admin@admin.ru', password: '123'},
   {name: 'Pasha', email: 'a@a.ru', password: '123'},
   {name: 'Olya', email: 'a@a.ru', password: '123'}
 ])
-category = Category.create!([
+categories = Category.create!([
   {title: 'programming'},
   {title: 'humor'},
   {title: 'other'}
 ])
-test = Test.create!([
-  {title: 'Ruby', level: 1, category_id: category[0].id, user_id: user[0].id},
-  {title: 'TypeScript', level: 1, category_id: category[0].id, user_id: user[0].id},
-  {title: 'IQ', level: 0, category_id: category[1].id, user_id: user[0].id}
+tests = Test.create!([
+  {title: 'Ruby', level: 1, category_id: categories[0].id, user_id: users[0].id},
+  {title: 'TypeScript', level: 1, category_id: categories[0].id, user_id: users[0].id},
+  {title: 'IQ', level: 0, category_id: categories[1].id, user_id: users[0].id}
 ])
-question = Question.create!([
-  {body: 'Что вернет следующие выражание? 57 / 0', test_id: test[0].id},
-  {body: 'Что такое TypeScript?', test_id: test[1].id},
-  {body: 'А можно пупок завязать обратно, если он развяжется?', test_id: test[2].id},
+questions = Question.create!([
+  {body: 'Что вернет следующие выражание? 57 / 0', test_id: tests[0].id},
+  {body: 'Что такое TypeScript?', test_id: tests[1].id},
+  {body: 'А можно пупок завязать обратно, если он развяжется?', test_id: tests[2].id},
 ])
-answer = Answer.create!([
-  {body: 'nil', question_id: question[0].id},
-  {body: '0', question_id: question[0].id},
-  {body: 'ZeroDivisionError (divided by 0)', question_id: question[0].id},
-  {body: 'Надстройка над JavaScript', question_id: question[1].id},
-  {body: 'Самостоятельный язык программирования', question_id: question[1].id},
-  {body: 'Препроцессор JavaScript', question_id: question[1].id},
-  {body: 'Да, морским улом', question_id: question[2].id},
-  {body: 'Нет', question_id: question[2].id},
-  {body: 'Он не развяжется', question_id: question[2].id}
+answers = Answer.create!([
+  {body: 'nil', question_id: questions[0].id},
+  {body: '0', question_id: questions[0].id},
+  {body: 'ZeroDivisionError (divided by 0)', question_id: questions[0].id},
+  {body: 'Надстройка над JavaScript', question_id: questions[1].id},
+  {body: 'Самостоятельный язык программирования', question_id: questions[1].id},
+  {body: 'Препроцессор JavaScript', question_id: questions[1].id},
+  {body: 'Да, морским улом', question_id: questions[2].id},
+  {body: 'Нет', question_id: questions[2].id},
+  {body: 'Он не развяжется', question_id: questions[2].id}
 ])
 
 Result.create!([
-  {user_id: user[1].id, test_id: test[0].id},
-  {user_id: user[1].id, test_id: test[1].id, status: true},
-  {user_id: user[1].id, test_id: test[2].id},
-  {user_id: user[2].id, test_id: test[2].id},
+  {user_id: users[1].id, test_id: tests[0].id},
+  {user_id: users[1].id, test_id: tests[1].id, status: true},
+  {user_id: users[1].id, test_id: tests[2].id},
+  {user_id: users[2].id, test_id: tests[2].id},
 ])
