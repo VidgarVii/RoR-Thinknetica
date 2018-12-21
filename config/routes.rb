e79864db9530d86d6fb465f8a722506c1c460797
VidgarVii <vidasp@mail.ru>
2018-12-21 16:52:21 +0300

    root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'tests/index'
-  get 'tests/show'
+  root 'tests#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tests do
     resources :questions, shallow: true, only: %i[show edit new destroy update create]
