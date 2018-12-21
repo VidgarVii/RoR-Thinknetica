0bece8cc1ea6e4ca53d0b7b7fd9976c9bfb188fd
VidaspVii <vidasp@mail.ru>
2018-12-21 22:48:39 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tests do
-    resources :questions, shallow: true, only: %i[show edit new destroy update create]
+    resources :questions, shallow: true, except: :index
   end
 end
