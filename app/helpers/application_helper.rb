0609ffc19cf7a54703072c2f9dd193b024a77839
VidaspVii <vidasp@mail.ru>
2018-12-21 22:55:59 +0300

    github_url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,6 @@ module ApplicationHelper
   end
 
   def github_url(author, repo)
-    link_to author, repo, target: '_blank'
+    link_to author, "https://github.com/#{author}/#{repo}", target: '_blank'
   end
 end
