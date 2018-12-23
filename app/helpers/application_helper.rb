d27138e010bdac0c9102a9d9b554906912f2850c
VidaspVii <vidasp@mail.ru>
2018-12-23 12:47:11 +0300

    refact helper header form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to author, "https://github.com/#{author}/#{repo}", target: '_blank'
   end
+
+  def header_form(object, name)
+    object.new_record? ? t("actions.creates.#{name}") : t("actions.update.#{name}")
+  end
 end
