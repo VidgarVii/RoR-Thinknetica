84de4fa2bc73360ba4546c9fd93f03b4d00c3d5d
VidaspVii <vidasp@mail.ru>
2018-12-23 12:57:26 +0300

    add destroy for test

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,5 @@
 class TestsController < ApplicationController
-  before_action :set_test, only: %i[show edit update]
+  before_action :set_test, only: %i[show edit update destroy]
 
   def index
     @tests = Test.all
@@ -32,6 +32,11 @@ class TestsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to tests_path
   end
 
   private
