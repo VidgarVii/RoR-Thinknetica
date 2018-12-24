0aceeab984d7ad39de64def6f936859647f70cc3
VidgarVii <vidasp@mail.ru>
2018-12-24 16:58:14 +0300

    next_quest

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -5,9 +5,17 @@ class TestPassagesController < ApplicationController
   end
 
   def result
+    render plain: 'END'
   end
 
   def update
+    @test_passage.accept!(params[:answer_ids])
+
+    if @test_passage.completed?
+      redirect_to result_test_passage_path(@test_passage)
+    else
+      render :show
+    end
   end
 
   private
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -5,9 +5,32 @@ class TestPassage < ApplicationRecord
 
   before_validation :before_validation_set_first_question, on: :create
 
+  def accept!(answer_ids)
+    self.correct_questions += 1 if correct_answers?(answer_ids)
+    self.current_question = next_question
+    save!
+  end
+
+  def completed?
+    current_question.nil?
+  end
+
   private
+
+  def correct_answers?(answer_ids)
+    count_answers = current_answers.size
+    count_answers == current_answers.where(id: answer_ids).size && count_answers == answer_ids.size
+  end
+
+  def current_answers
+    current_question.answers.true_answers
+  end
 
   def before_validation_set_first_question
     self.current_question = test.questions.first if test.present?
+  end
+
+  def next_question
+    test.questions.order(:id).where('id > ?', current_question.id).first
   end
 end
