05a19a008ffed8a410e50f5ba023879062ac09fe
VidgarVii <vidasp@mail.ru>
2018-12-25 13:19:26 +0300

    views

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,2 +1,11 @@
 module TestPassagesHelper
+
+  def result_test(correct_questions, total_count_questions)
+    result = correct_questions*100/total_count_questions
+
+    return "<span style='color:#4caf50'>Результат #{result}% правильных ответов.
+             Тест пройден</span>".html_safe if result >= 85
+    return "<span style='color:#f00'>Результат #{result}% правильных ответов.
+             Тест не пройден</span>".html_safe if result < 85
+  end
 end
