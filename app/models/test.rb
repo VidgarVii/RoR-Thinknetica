671dd1b30cf28e53978e5a6251fa5c146250c0cb
Pavel <vidasp@mail.ru>
2019-01-14 15:32:15 +0300

    fix destroy test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   has_many :questions, dependent: :destroy
-  has_many :test_passages
+  has_many :test_passages, dependent: :destroy
   has_many :users, through: :test_passages
   belongs_to :category, optional: true
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
