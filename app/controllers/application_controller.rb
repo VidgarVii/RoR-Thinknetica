3fcbdfa9108f5b90a055d67320f64fc0491c0e39
VidaspVii <vidasp@mail.ru>
2019-01-14 21:15:44 +0300

    Hello <name>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(user)
+    flash[:notice] = "Привет #{user.first_name}"
     user.admin? ? admin_tests_path : tests_path
   end
 
