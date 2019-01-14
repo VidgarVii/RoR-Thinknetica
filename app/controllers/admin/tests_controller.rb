e8bc696b08c3740d9236d255b803d1b65321c1d4
Pavel <vidasp@mail.ru>
2019-01-14 16:59:26 +0300

    admin author test

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -16,7 +16,7 @@ class Admin::TestsController < Admin::BaseController
 
   def create
     @test = Test.new(test_params)
-    @test.author = User.first #Временная заглушка
+    @test.author = current_user
 
     if @test.save
       redirect_to admin_tests_path
