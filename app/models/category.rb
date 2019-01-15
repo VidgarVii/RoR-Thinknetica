7d1ecbb6ce39fe0a06c11ed24f6e52712d81801c
Pavel <vidasp@mail.ru>
2019-01-15 16:30:05 +0300

    i18n collection

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ApplicationRecord
   default_scope { order(:title) }
   has_many :tests, dependent: :nullify
   validates :title, presence: true
+
+  def translated_title
+    I18n.t(title, scope: 'categories')
+  end
 end
