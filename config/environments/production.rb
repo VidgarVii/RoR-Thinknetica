f12c24261e85e8e81d909452b017c7fbadbae7df
Pavel <vidasp@mail.ru>
2019-01-21 14:10:45 +0300

    fix smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'testsguru.herokuapp.com'}
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_setting = {
+  config.action_mailer.smtp_settings = {
       address:               'smtp.gmail.com',
       port:                  587,
       user_name:             ENV['SMTP_USERNAME'],
