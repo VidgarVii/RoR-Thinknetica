cce5e69e353e80a2575fea55277024933e75d840
VidaspVii <vidasp@mail.ru>
2019-01-23 22:25:23 +0300

    authenticate_user!

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,4 +1,6 @@
 class BadgesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @current_badges = current_user.badges
     @badges = Badge.where.not(id: @current_badges.ids)
