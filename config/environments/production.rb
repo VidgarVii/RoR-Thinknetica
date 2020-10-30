e45b3f51e6269df223ddedef3629360d6db3238e
Pavel Rebrov <vidasp@mail.ru>
2020-10-30 14:04:44 +0300

    Update smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      address:               'smtp.gmail.com',
+      address:               ENV['SMTP_SERVER'],
       port:                  587,
       user_name:             ENV['SMTP_USERNAME'],
       password:              ENV['SMTP_PASSWORD'],
